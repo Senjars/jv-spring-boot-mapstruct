e35d5cdecd03ee663cba5c684a21d84501660510
Senjars <jaroslawsendecki1@gmail.com>
2026-01-04 23:31:58 +0100

    First solution

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,45 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
+import mate.academy.mapstruct.model.Group;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+@Mapper(componentModel = "spring")
 public interface StudentMapper {
-    StudentDto toDto(Student student);
 
-    StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
+    default StudentDto toDto(Student student) {
+        StudentDto studentDto = new StudentDto();
 
+        studentDto.setId(student.getId());
+        studentDto.setName(student.getName());
+        studentDto.setEmail(student.getEmail());
+        studentDto.setGroupId(student.getGroup() != null ? student.getGroup().getId() : null);
+        studentDto.setSubjectIds(student.getSubjects() != null
+                ? student.getSubjects().stream().map(Subject::getId).collect(Collectors.toList())
+                : Collections.emptyList());
+        return studentDto;
+    }
+
+    default StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student) {
+        StudentWithoutSubjectsDto studentWithoutSubjectsDto = new StudentWithoutSubjectsDto();
+        studentWithoutSubjectsDto.setGroupId(student.getGroup().getId());
+        studentWithoutSubjectsDto.setId(student.getId());
+        studentWithoutSubjectsDto.setEmail(student.getEmail());
+        studentWithoutSubjectsDto.setName(student.getName());
+        return studentWithoutSubjectsDto;
+    }
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "group", ignore = true)
+    @Mapping(target = "subjects", ignore = true)
+    @Mapping(target = "socialSecurityNumber", ignore = true)
     Student toModel(CreateStudentRequestDto requestDto);
 }
